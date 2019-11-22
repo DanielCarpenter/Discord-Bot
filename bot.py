667ac2eecc72a36578a007a0967de204a8c0b9c1
Daniel Carpenter <d_c176@txstate.edu>
2019-11-22 02:34:24 -0600

    secret santa implementation

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 # bot.py
 import os
 import random
+import copy
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
@@ -11,33 +12,34 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 bot = commands.Bot(command_prefix='!')
 
 sslist = []
-giftee = ['nobody']
+names = sslist
+gifted = []
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
 
 @bot.command()
-async def ssenroll(ctx):
+async def enroll(ctx):
     for i in sslist:
         if(i == ctx.author):
             await ctx.send("{} already enrolled".format(ctx.author))
+            print(sslist[0])
             return
     sslist.append(ctx.author)
-    giftee.append(ctx.author)
+    gifted.append(ctx.author)
     await ctx.send('{} enrolled in Secret Santa'.format(ctx.author))
 
 @bot.command()
 async def ss(ctx):
-    if len(sslist) < 4:
-        await ctx.author.send("Insufficient Participants")
+    if (len(names) < 4):
+        await ctx.send("Insufficient Participants")
     else:
-        seed = random.seed()
-        while len(sslist) and len(giftee):
-            gifter = random.randint(0,len(sslist))
-            gifte = gifter
-            while gifte == gifter:
-                gifte = random.randint(0,len(sslist))
-            await sslist.pop(gifter).send('You are gifting {}'.format(giftee.pop(gifte)))
+        for i in names:
+            sslist = copy.copy(names)
+            gifter = sslist.pop(sslist.index(i))
+            giftee = random.choice(list(set(gifted)&set(sslist)))
+            gifted.remove(giftee)
+            await gifter.send('You are gifting {}'.format(giftee))
 
 
 
