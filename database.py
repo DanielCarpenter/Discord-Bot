007cd85dc8c62e35b3c85d1a26a9f2344e79401f
Daniel Carpenter <d_c176@txstate.edu>
2019-11-25 21:53:57 -0600

    Created: database.py, utilty.py, and bot_test.py

---
+++ database.py
@@ -0,0 +1,4 @@
+"""database.py:
+creation of SQLAlchemy functions and models go here
+"""
+from flask_sqlalchemy import SQLAlchemy
---
+++ utility.py
@@ -0,0 +1,7 @@
+
+"""
+utility.py:
+Functions that do not directly impact bot commands go here
+"""
+from datetime import datetime, date, timedelta
+
